f626c31af620915b4ffa0e4bb8d2b151e57e9485
Hannah Booth <hannahjbooth9@gmail.com>
2024-08-07 17:54:19 +0100

    edited home page nav bar and added JS functionality to Services button according to user scrolling

--- script.js
+++ script.js
@@ -10,4 +10,20 @@ Array.from(links).forEach(function(link) {
     }
 });
 
+// handle the underscore for Sections link according to user scrolling
+const servicesSection = document.getElementById('services');
+const servicesLink = document.getElementById('servces-link');
+
+window.addEventListener('scroll', function() {
+    const sectionTop = servicesSection.getBoundingClientRect().top;
+    const sectionHeight = servicesSection.offsetHeight;
+    const viewportHeight = window.innerHeight;
+    const isInViewport = sectionTop < viewportHeight && (sectionTop + sectionHeight) > 0;
+
+    if (isInViewport) {
+        servicesLink.parentElement.classList.add('current');
+    } else {
+        servicesLink.parentElement.classList.remove('current');
+    }
+});
 
