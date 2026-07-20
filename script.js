07e81b062eb524016be30e7ddd23c56aa2a95cba
Hannah Booth <hannahjbooth9@gmail.com>
2026-07-20 09:30:32 +0100

    add content and accordeon logic to policies page

--- script.js
+++ script.js
@@ -1,61 +1,64 @@
 // Add underscore to the current page in the navigation bar
-const links = document.getElementsByClassName('underscore'); // get all a tags to be underscored in an array
+const links = document.getElementsByClassName("underscore"); // get all a tags to be underscored in an array
 const currentUrl = window.location.href;
 
 // Check if the current URL includes a fragment
 const currentFragment = window.location.hash;
 
-Array.from(links).forEach(function(link) {
-    // Remove fragment part for comparison
-    const linkHref = link.href.split('#')[0];
+Array.from(links).forEach(function (link) {
+  // Remove fragment part for comparison
+  const linkHref = link.href.split("#")[0];
 
-    if (linkHref === currentUrl.split('#')[0]) {
-        link.parentElement.classList.add('current');
-    } else {
-        // Otherwise, remove 'current' class
-        link.parentElement.classList.remove('current');
-    }
+  if (linkHref === currentUrl.split("#")[0]) {
+    link.parentElement.classList.add("current");
+  } else {
+    // Otherwise, remove 'current' class
+    link.parentElement.classList.remove("current");
+  }
 
-    // Special case for 'terms' fragment
-    if (currentFragment === '#terms' && linkHref.includes('prices.html')) {
-        // Underline the 'Prices' link if the fragment is '#terms'
-        link.parentElement.classList.add('current');
-    }
-}); 
+  // Special case for 'terms' fragment
+  if (currentFragment === "#terms" && linkHref.includes("prices.html")) {
+    // Underline the 'Prices' link if the fragment is '#terms'
+    link.parentElement.classList.add("current");
+  }
+});
 
 // Handle the underscore for Sections link according to user scrolling
-const servicesSection = document.getElementById('services');
-const servicesLink = document.getElementById('services-link');
-const homeLink = document.getElementById('home-link');
-const footer = document.querySelector('footer'); // Update with the correct selector for your footer
+const servicesSection = document.getElementById("services");
+const servicesLink = document.getElementById("services-link");
+const homeLink = document.getElementById("home-link");
+const footer = document.querySelector("footer"); // Update with the correct selector for your footer
 
 // Scroll handler function
 function handleScroll() {
-    const sectionTop = servicesSection.getBoundingClientRect().top;
-    const sectionHeight = servicesSection.offsetHeight;
-    const footerTop = footer.getBoundingClientRect().top;
-    const viewportHeight = window.innerHeight;
+  const sectionTop = servicesSection.getBoundingClientRect().top;
+  const sectionHeight = servicesSection.offsetHeight;
+  const footerTop = footer.getBoundingClientRect().top;
+  const viewportHeight = window.innerHeight;
 
-    const isInServicesViewport = sectionTop < viewportHeight && (sectionTop + sectionHeight) > 0;
-    const isInFooterViewport = footerTop < viewportHeight && footerTop > 0;
+  const isInServicesViewport =
+    sectionTop < viewportHeight && sectionTop + sectionHeight > 0;
+  const isInFooterViewport = footerTop < viewportHeight && footerTop > 0;
 
-    if (isInFooterViewport) {
-        servicesLink.parentElement.classList.remove('current');
-        homeLink.parentElement.classList.add('current');
-    } else if (isInServicesViewport) {
-        servicesLink.parentElement.classList.add('current');
-        homeLink.parentElement.classList.remove('current');
-    } else {
-        servicesLink.parentElement.classList.remove('current');
-        homeLink.parentElement.classList.add('current');
-    }
+  if (isInFooterViewport) {
+    servicesLink.parentElement.classList.remove("current");
+    homeLink.parentElement.classList.add("current");
+  } else if (isInServicesViewport) {
+    servicesLink.parentElement.classList.add("current");
+    homeLink.parentElement.classList.remove("current");
+  } else {
+    servicesLink.parentElement.classList.remove("current");
+    homeLink.parentElement.classList.add("current");
+  }
 }
 
 // Add scroll event listener
-window.addEventListener('scroll', handleScroll);
+window.addEventListener("scroll", handleScroll);
 
 // Call the scroll handler on page load to underscore the correct link
-handleScroll();
+if (document.querySelector("main.main-home")) {
+  handleScroll();
+}
 
 // // add underscore to current page in navigation bar
 // const links = document.getElementsByClassName('underscore'); // get all 3 a tags to be underscored in an array
@@ -80,14 +83,13 @@ handleScroll();
 //         // Underline the 'Prices' link if the fragment is '#terms'
 //         link.parentElement.classList.add('current');
 //     }
-// }); 
+// });
 
 // // handle the underscore for Sections link according to user scrolling
 // const servicesSection = document.getElementById('services');
 // const servicesLink = document.getElementById('services-link');
 // const homeLink = document.getElementById('home-link');
 // const footer = document.querySelector('footer'); // Update with the correct selector for your footer
-
 
 // window.addEventListener('scroll', function() {
 //     const sectionTop = servicesSection.getBoundingClientRect().top;
@@ -112,44 +114,68 @@ handleScroll();
 
 // --- Contact Form
 
-document.addEventListener("DOMContentLoaded", function() {
+function handleContactForm() {
+  document.addEventListener("DOMContentLoaded", function () {
     // Get the elements from the DOM
     const homeAddressInput = document.getElementById("home-address");
     const physioAddressInput = document.getElementById("physio-address");
     const sameAddressCheckbox = document.getElementById("same-address");
 
     // Add an event listener to the checkbox
-    sameAddressCheckbox.addEventListener("change", function() {
-        if (this.checked) {
-            // Copy the home address to the physio address input field
-            physioAddressInput.value = homeAddressInput.value;
-        } else {
-            // If the checkbox is unchecked, clear the physio address field and re-enable it
-            physioAddressInput.value = "";
-        }
+    sameAddressCheckbox.addEventListener("change", function () {
+      if (this.checked) {
+        // Copy the home address to the physio address input field
+        physioAddressInput.value = homeAddressInput.value;
+      } else {
+        // If the checkbox is unchecked, clear the physio address field and re-enable it
+        physioAddressInput.value = "";
+      }
     });
 
     // Optionally, update the physio address field if the home address is changed while the checkbox is checked
-    homeAddressInput.addEventListener("input", function() {
-        if (sameAddressCheckbox.checked) {
-            physioAddressInput.value = homeAddressInput.value;
-        }
+    homeAddressInput.addEventListener("input", function () {
+      if (sameAddressCheckbox.checked) {
+        physioAddressInput.value = homeAddressInput.value;
+      }
     });
-});
+  });
 
-// add a new textarea when physiotherapy, hydrotherapy or mot are selected
+  // add a new textarea when physiotherapy, hydrotherapy or mot are selected
 
-document.getElementById('services').addEventListener('change', function() {
+  document.getElementById("services").addEventListener("change", function () {
     const selectedValue = this.value;
-    const textarea = document.getElementById('dynamic-textarea');
+    const textarea = document.getElementById("dynamic-textarea");
 
     // Check if the selected value requires a textarea
-    if (selectedValue === 'physiotherapy' || selectedValue === 'hydrotherapy' || selectedValue === 'mot') {
-        // Show the textarea
-        textarea.style.display = 'block';
+    if (
+      selectedValue === "physiotherapy" ||
+      selectedValue === "hydrotherapy" ||
+      selectedValue === "mot"
+    ) {
+      // Show the textarea
+      textarea.style.display = "block";
     } else {
-        // Hide the textarea
-        textarea.style.display = 'none';
-        textarea.value = ''; // Clear the textarea when hiding
+      // Hide the textarea
+      textarea.style.display = "none";
+      textarea.value = ""; // Clear the textarea when hiding
     }
-});
+  });
+}
+
+if (document.querySelector("main.main-contact")) {
+  handleContactForm();
+}
+
+// Accordeon
+
+let titles = Array.from(document.getElementsByClassName("accordeon-button"));
+console.log("titles", titles);
+
+for (let title of titles) {
+  title.addEventListener("click", () => {
+    let parent = title.parentElement;
+    let content = parent.querySelector(".accordeon-content");
+
+    content.classList.toggle("visible");
+  });
+}
